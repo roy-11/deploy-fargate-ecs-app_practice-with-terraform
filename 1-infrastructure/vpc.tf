d9132fc5614641df6d2eac7fc7d008e602ec8ffe
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-04-26 16:59:27 +0900

    create public subnet

--- 1-infrastructure/vpc.tf
+++ 1-infrastructure/vpc.tf
@@ -14,3 +14,33 @@ resource "aws_vpc" "production-vpc" {
     Name = "Production-VPC"
   }
 }
+
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id            = "${aws_vpc.production-vpc.id}"
+  cidr_block        = "${var.public_subnet_1_cider}"
+  availability_zone = "ap-northeast-1a"
+
+  tags {
+    Name = "Public-Subnet-1"
+  }
+}
+
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id            = "${aws_vpc.production-vpc.id}"
+  cidr_block        = "${var.public_subnet_2_cider}"
+  availability_zone = "ap-northeast-1c"
+
+  tags {
+    Name = "Public-Subnet-2"
+  }
+}
+
+resource "aws_subnet" "public-subnet-3" {
+  vpc_id            = "${aws_vpc.production-vpc.id}"
+  cidr_block        = "${var.public_subnet_3_cider}"
+  availability_zone = "ap-northeast-1d"
+
+  tags {
+    Name = "Public-Subnet-3"
+  }
+}
